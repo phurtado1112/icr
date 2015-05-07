c48a148df8b7cba6e03e111a63abdf0d6d49fe4d
phurtado1112 <phurtado1112@gmail.com>
2015-05-06 21:40:55 -0600

    Merge branch 'master' of https://github.com/phurtado1112/cuentas
    Conflicts:
    	catalogos/MetodosCatalogos.php
    	catalogos/categoriascuentas/CategoriasCuentas.php
    	catalogos/categoriascuentas/categorias_cuentas_crear.php
    	catalogos/categoriascuentas/categorias_cuentas_procesar.php

--- administracion/acceso_usuario_proceso.php
+++ administracion/acceso_usuario_proceso.php
@@ -19,26 +19,26 @@ $lista_usuario = bd_ejecutar_sql($consulta_usuario);
 $fila = bd_obtener_fila($lista_usuario);
 
 if (!$fila[0]) { //opcion1: Si el usuario NO existe o los datos son INCORRRECTOS
-    echo '<script language = javascript>
-	alert("Usuario o Contraseña errados o no tiene privilegios, por favor verifique")
-	self.location = "index.php"
-	</script>';
+    echo '<script language = "javascript">
+          alert("Usuario o Contraseña errados o no tiene privilegios");
+          </script>';
+    header("Location: index.php");
 } else { //opcion2: Usuario logueado correctamente
     //Definimos las variables de sesión y redirigimos a la p�gina de usuario
     $_SESSION['nombre_usuario'] = $fila['nombre'];
     $_SESSION['idusuario'] = $fila['idusuario'];
 
     // Se busca y elimina cualquier sesión anterior hecha por el usuario
-    $consulta_sesion_anterior="select * from session where idusuario= '".$_SESSION['idusuario']."'";
+    $consulta_sesion_anterior = "select * from session where idusuario= '" . $_SESSION['idusuario'] . "'";
     $lista_sesion_anterior = bd_ejecutar_sql($consulta_sesion_anterior);
     $fila_sesion_anterior = bd_obtener_fila($lista_sesion_anterior);
     $sesion_anterior = $fila_sesion_anterior['idusuario'];
-    
-    if(isset($sesion_anterior)){
-        $consulta_eliminar_sesion_anterior = "delete from session where idusuario = '".$sesion_anterior."'";
+
+    if (isset($sesion_anterior)) {
+        $consulta_eliminar_sesion_anterior = "delete from session where idusuario = '" . $sesion_anterior . "'";
         bd_ejecutar_sql($consulta_eliminar_sesion_anterior);
     }
-    
+
     $consulta_sesion = "INSERT INTO session (session_id,idusuario,usuario)
 			VALUES(
 			'" . session_id() . "',

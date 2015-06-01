6c7823550ab81bccc7ca21b05087aa5cc4f6ed07
phurtado1112 <phurtado1112@gmail.com>
2015-06-01 12:07:56 -0600

    Merge branch 'master' of https://github.com/phurtado1112/cuentas
    Conflicts:
    	catalogos/MetodosCatalogos.php
    	catalogos/categoriascuentas/CategoriasCuentas.php
    	catalogos/categoriascuentas/categorias_cuentas_crear.php
    	catalogos/categoriascuentas/categorias_cuentas_procesar.php

--- administracion/reportes/phprptinc/menu.php
+++ administracion/reportes/phprptinc/menu.php
@@ -8,6 +8,7 @@ $RootMenu->IsRoot = TRUE;
 $RootMenu->AddMenuItem(50, $ReportLanguage->Phrase("DetailSummaryReportMenuItemPrefix") . $ReportLanguage->MenuPhrase("50", "MenuText") . $ReportLanguage->Phrase("DetailSummaryReportMenuItemSuffix"), "gestif3n_campaf1a_x_programa_open_exedsmry.php", -1, "", TRUE, FALSE);
 $RootMenu->AddMenuItem(51, $ReportLanguage->Phrase("DetailSummaryReportMenuItemPrefix") . $ReportLanguage->MenuPhrase("51", "MenuText") . $ReportLanguage->Phrase("DetailSummaryReportMenuItemSuffix"), "gestif3n_campaf1a_x_asesor_open_exedsmry.php", -1, "", TRUE, FALSE);
 $RootMenu->AddMenuItem(56, $ReportLanguage->Phrase("DetailSummaryReportMenuItemPrefix") . $ReportLanguage->MenuPhrase("56", "MenuText") . $ReportLanguage->Phrase("DetailSummaryReportMenuItemSuffix"), "gestif3n_detalle_de_campaf1a_x_asesor_open_exedsmry.php", -1, "", TRUE, FALSE);
+$RootMenu->AddMenuItem(62, $ReportLanguage->Phrase("DetailSummaryReportMenuItemPrefix") . $ReportLanguage->MenuPhrase("62", "MenuText") . $ReportLanguage->Phrase("DetailSummaryReportMenuItemSuffix"), "actualizacif3n_x_asesor_x_campaf1asmry.php", -1, "", TRUE, FALSE);
 $RootMenu->Render();
 ?>
 </div>
